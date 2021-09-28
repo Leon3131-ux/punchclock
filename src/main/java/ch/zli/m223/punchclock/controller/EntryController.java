6adf5a3bca2323df7bc4160b787d51462326864e
Leon Domiter <leon.domiter@hotmail.com>
2021-09-28 11:28:06 +0200

    added validation and crud for entry

--- src/main/java/ch/zli/m223/punchclock/controller/EntryController.java
+++ src/main/java/ch/zli/m223/punchclock/controller/EntryController.java
@@ -6,9 +6,11 @@ import ch.zli.m223.punchclock.domain.User;
 import ch.zli.m223.punchclock.dto.EntryDto;
 import ch.zli.m223.punchclock.service.EntryService;
 import ch.zli.m223.punchclock.service.UserService;
+import ch.zli.m223.punchclock.validator.EntryValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -23,6 +25,10 @@ public class EntryController {
     private final EntryService entryService;
     private final EntryDtoConverter entryDtoConverter;
     private final UserService userService;
+    private final EntryValidator entryValidator;
+
+    @InitBinder("entryDto")
+    public void setEntryDtoBinder(WebDataBinder binder){binder.setValidator(entryValidator);}
 
 
     @GetMapping("/api/entries")
@@ -40,10 +46,24 @@ public class EntryController {
         return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 
-    @RequestMapping(value = "/entry/{id}", method = RequestMethod.DELETE)
-    public ResponseEntity<?> deleteEntry(@PathVariable("id") Long id){
+    @RequestMapping(value = "/api/entry", method = RequestMethod.PUT)
+    public ResponseEntity<?> updateEntry(@Valid @RequestBody EntryDto entryDto, Principal principal){
+        User user = userService.getByUsernameOrElseThrow(principal.getName());
+        if(entryDto.getId() != null && entryDto.getId() != 0){
+            Entry entry = entryDtoConverter.toEntity(entryDto, user);
+            Optional<Entry> oldEntry = entryService.findById(entryDto.getId());
+            if(oldEntry.isPresent() && oldEntry.get().getUser().equals(user)){
+                return new ResponseEntity<>(entryDtoConverter.toDto(entryService.updateEntry(oldEntry.get(), entry)), HttpStatus.OK);
+            }
+        }
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+    @RequestMapping(value = "/api/entry/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<?> deleteEntry(@PathVariable("id") Long id, Principal principal){
+        User user = userService.getByUsernameOrElseThrow(principal.getName());
         Optional<Entry> oldEntry = entryService.findById(id);
-        if(oldEntry.isPresent()){
+        if(oldEntry.isPresent() && oldEntry.get().getUser().equals(user)){
             entryService.deleteEntry(oldEntry.get());
             return new ResponseEntity<>(HttpStatus.OK);
         }else {
---
+++ src/main/java/ch/zli/m223/punchclock/validator/EntryValidator.java
@@ -0,0 +1,43 @@
+package ch.zli.m223.punchclock.validator;
+
+import ch.zli.m223.punchclock.domain.Entry;
+import ch.zli.m223.punchclock.dto.EntryDto;
+import ch.zli.m223.punchclock.repository.EntryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class EntryValidator implements Validator {
+
+    private final EntryRepository entryRepository;
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return clazz.isAssignableFrom(EntryDto.class);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        EntryDto dto = (EntryDto) target;
+
+        if(dto.getId() != null && dto.getId() != 0){
+            Optional<Entry> oldEntry = entryRepository.findById(dto.getId());
+            if(oldEntry.isEmpty()){
+                errors.rejectValue("id", "errors.entry.id.invalid");
+            }
+        }
+
+        if(dto.getCheckIn() != null && dto.getCheckOut() != null){
+            if(dto.getCheckIn().isAfter(dto.getCheckOut())){
+                errors.rejectValue("checkIn", "errors.entry.checkIn.invalid");
+            }else if(dto.getCheckOut().isBefore(dto.getCheckIn())){
+                errors.rejectValue("checkOut", "errors.entry.checkOut.invalid");
+            }
+        }
+    }
+}
